b75791d49a695932d26d5b93d0af4b0ed116147f
lsprr <16653744+lsprr@users.noreply.github.com>
2023-04-19 19:25:16 -0400

    Update Creatures page

--- frontend/src/pages/creatures.tsx
+++ frontend/src/pages/creatures.tsx
@@ -1,7 +1,8 @@
+import Image from 'next/image';
 import { fetchData } from '@/utils/api';
 import Filter from '@/components/Filter';
 import { useEffect, useState } from 'react';
-import Image from 'next/image';
+import anchBg from '@assets/images/background.jpg';
 
 export default function Creatures() {
     const [fish, setFish] = useState(null);
@@ -13,19 +14,22 @@ export default function Creatures() {
     useEffect(() => {
         fetchData('creatures')
             .then((data) => {
+                setDisplayedItems(data['Fish']);
                 setFish(data['Fish']);
                 setInsects(data['Insects']);
                 setSeaCreatures(data['Sea Creatures']);
-                setDisplayedItems(data['Fish']);
             })
             .catch((error) => {
                 console.error('Error fetching creatures:', error);
             });
     }, []);
 
+
     const handleFilterKeywordChange = (value: string) => {
         setFilterKeyword(value);
     };
+
+    console.log(displayedItems);
 
     const handleDisplayedItemsChange = (value: string) => {
         switch (value) {
@@ -44,24 +48,31 @@ export default function Creatures() {
     };
 
     const renderItem = (item: any) => (
-        <li key={item.name} className='bg-[#e7dfae] rounded'>
-            <a href="#" className="group block overflow-hidden">
-                <Image
-                    src={item.iconImage}
-                    alt={item.description[0]}
-                    className="h-[350px] object-contain transition duration-500 group-hover:scale-105 m-auto"
-                    width={100}
-                    height={350}
-                    loading='lazy'
-                />
-                <div className="relative bg-[#2F3939] dark:bg-[#F5EADD] p-3">
+        <li key={item.name} className='rounded relative'>
+            <Image src={anchBg} alt="Background image" className="z-0" fill />
+            <div className="z-10 relative">
+                <div className="relative p-3">
                     <h1
-                        className="text-[#FAEADD] dark:text-[#2F3939] capitalize text-center"
+                        className="capitalize italic text-center anchBorder relative z-10 whitespace-nowrap shadow-md"
                     >
                         {item.name}
                     </h1>
                 </div>
-            </a>
+                <div className='group block overflow-hidden cursor-pointer'>
+                    <Image
+                        src={item.critterpediaImage}
+                        alt={item.catchPhrase[0]}
+                        className="h-[300px] object-contain transition duration-500 group-hover:scale-150 m-auto"
+                        width={100}
+                        height={350}
+                        loading='lazy'
+                    />
+                </div>
+                <div className='flex justify-start'>
+                    <Image src={item.furnitureImage} alt={item.description[0]} width={50} height={50} className='mr-2' />
+                    <Image src={item.iconImage} alt={item.description[0]} width={50} height={50} />
+                </div>
+            </div>
         </li>
     );
 
@@ -71,13 +82,13 @@ export default function Creatures() {
                 <p className="block text-[#2F3939] dark:text-[#F5EADD]">Filters</p>
                 <div className="mt-1 space-y-2">
                     <div
-                        className="bg-[#2F3939] dark:bg-[#F5EADD] overflow-hidden rounded border border-gray-300 [&_summary::-webkit-details-marker]:hidden"
+                        className="bg-[#2F3939] dark:bg-[#F5EADD] overflow-hidden rounded border border-[#2F3939] dark:border-[#F5EADD] [&_summary::-webkit-details-marker]:hidden"
                     >
                         <div className="flex items-center justify-between gap-2 p-4 text-white dark:text-[#2F3939] transition">
                             <span>Aquatic</span>
                         </div>
-                        <div className="border-t border-gray-200 bg-[#F5EADD] dark:bg-[#2F3939]">
-                            <ul className="space-y-1 border-t border-gray-200 p-4">
+                        <div className="border-t border-[#2F3939] bg-[#F5EADD] dark:bg-[#2F3939]">
+                            <ul className="space-y-1 border-t border-[#2F3939] p-4">
                                 <li>
                                     <label
                                         htmlFor="fish"
