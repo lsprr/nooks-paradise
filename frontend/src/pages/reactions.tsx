ada42b2ac001031e7570e94439242efcad74c431
lsprr <16653744+lsprr@users.noreply.github.com>
2023-04-22 21:51:11 -0400

    Update Reactions page

--- frontend/src/pages/reactions.tsx
+++ frontend/src/pages/reactions.tsx
@@ -1,19 +1,105 @@
+import { fetchData } from '@/utils/api';
+import { mergeAllArrays } from '@utils/arrayUtils';
+import { toCommas } from '@/utils/numberWithCommas';
+import Filter from '@/components/Filter';
 import { useEffect, useState } from 'react';
+import { Card } from '@/components/Card';
+import acnhBg from '@assets/images/background.jpg';
+import acnhQuestion from '@assets/images/question.png';
+
+type StaticImageData = {
+    src: string;
+    height: number;
+    width: number;
+    blurDataURL?: string;
+}
+
+type Reactions = {
+    source: string;
+    name: string;
+    image: StaticImageData;
+};
 
 export default function Reactions() {
-    const [category, setCategory] = useState({});
+    const [displayedItems, setDisplayedItems] = useState<Array<any> | null>(null);
+    const [filterKeyword, setFilterKeyword] = useState('');
+    const [isLoading, setIsLoading] = useState(true);
+    const [errorMessage, setErrorMessage] = useState('');
 
     useEffect(() => {
-        fetch('http://localhost:3001/api/reactions')
-            .then((response) => response.json())
+        fetchData('reactions')
             .then((data) => {
-                setCategory({ data })
+                setDisplayedItems(mergeAllArrays(data));
+                setIsLoading(false);
+            })
+            .catch((error) => {
+                setErrorMessage('Error fetching reactions. Please try again.');
+                console.error('Error fetching reactions:', error);
             });
     }, []);
 
+    const handleFilterKeywordChange = (value: string) => {
+        setFilterKeyword(value);
+    };
+
+    const renderItem = (item: Reactions) => {
+        const infoElements = [
+            {
+                src: acnhQuestion,
+                alt: 'How to get the source',
+                text: `${item.source}`,
+            },
+        ];
+
+        return (
+            <Card
+                name={item.name}
+                image={item.image}
+                backgroundImage={acnhBg}
+                infoElements={infoElements}
+            />
+        );
+    };
+
+    const renderFilters = () => {
+        return (
+            <div></div>
+        );
+    };
+
+    const FilteredItemsWithLoadingIndicator = () => {
+        if (errorMessage) {
+            return (
+                <div className="text-red-500 flex justify-center">
+                    {errorMessage}
+                </div>
+            );
+        } else {
+            return (
+                <>
+                    {isLoading ? (
+                        <div className="flex items-center justify-center space-x-2 animate-bounce">
+                            <div className="w-8 h-8 bg-blue-400 rounded-full"></div>
+                            <div className="w-8 h-8 bg-green-400 rounded-full"></div>
+                            <div className="w-8 h-8 bg-black rounded-full"></div>
+                        </div>
+                    ) : (
+                        <Filter
+                            displayedItems={displayedItems}
+                            filterKeyword={filterKeyword}
+                            onFilterKeywordChange={handleFilterKeywordChange}
+                            renderItem={renderItem}
+                            filters={renderFilters()}
+                        />
+                    )}
+                </>
+            );
+        }
+    }
+
     return (
         <>
-            <h1>Reactions</h1>
+            {FilteredItemsWithLoadingIndicator()}
         </>
     );
 }
