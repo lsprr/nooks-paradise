22ad68fcaa77a040ee00e3b65acba6f295e1be4e
lsprr <16653744+lsprr@users.noreply.github.com>
2023-04-22 21:51:59 -0400

    Update Reactions page

--- frontend/src/pages/reactions.tsx
+++ frontend/src/pages/reactions.tsx
@@ -1,6 +1,5 @@
 import { fetchData } from '@/utils/api';
 import { mergeAllArrays } from '@utils/arrayUtils';
-import { toCommas } from '@/utils/numberWithCommas';
 import Filter from '@/components/Filter';
 import { useEffect, useState } from 'react';
 import { Card } from '@/components/Card';
