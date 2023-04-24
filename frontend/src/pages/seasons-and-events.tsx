b9bd9753fd32ef6882c53ba9da903636181542dc
lsprr <16653744+lsprr@users.noreply.github.com>
2023-04-23 21:56:44 -0400

    Installing heroicons

--- frontend/src/pages/seasons-and-events.tsx
+++ frontend/src/pages/seasons-and-events.tsx
@@ -1,79 +1,19 @@
-import { fetchData } from '@/utils/api';
-import { mergeAllArrays } from '@utils/arrayUtils';
-import { Filter } from '@/components/Filter';
 import { useEffect, useState } from 'react';
 
-type SeasonAndEvent = {
-
-};
-
 export default function SeasonsAndEvents() {
-    const [displayedItems, setDisplayedItems] = useState<Array<any> | null>(null);
-    const [filterKeyword, setFilterKeyword] = useState('');
-    const [isLoading, setIsLoading] = useState(true);
-    const [errorMessage, setErrorMessage] = useState('');
+    const [category, setCategory] = useState({});
 
     useEffect(() => {
-        fetchData('seasonsandevents')
+        fetch('http://localhost:3001/api/seasonsandevents')
+            .then((response) => response.json())
             .then((data) => {
-                setDisplayedItems(mergeAllArrays(data));
-                setIsLoading(false);
-            })
-            .catch((error) => {
-                setErrorMessage('Error fetching Seasons and Events. Please try again.');
-                console.error('Error fetching Seasons and Events:', error);
+                setCategory({ data })
             });
     }, []);
 
-    const handleFilterKeywordChange = (value: string) => {
-        setFilterKeyword(value);
-    };
-
-    const renderItem = (item: SeasonAndEvent) => {
-        return (
-
-        );
-    };
-
-    const renderFilters = () => {
-        return (
-            <div></div>
-        );
-    };
-
-    const FilteredItemsWithLoadingIndicator = () => {
-        if (errorMessage) {
-            return (
-                <div className="text-red-500 flex justify-center">
-                    {errorMessage}
-                </div>
-            );
-        } else {
-            return (
-                <>
-                    {isLoading ? (
-                        <div className="flex items-center justify-center space-x-2 animate-bounce">
-                            <div className="w-8 h-8 bg-blue-400 rounded-full"></div>
-                            <div className="w-8 h-8 bg-green-400 rounded-full"></div>
-                            <div className="w-8 h-8 bg-black rounded-full"></div>
-                        </div>
-                    ) : (
-                        <Filter
-                            displayedItems={displayedItems}
-                            filterKeyword={filterKeyword}
-                            onFilterKeywordChange={handleFilterKeywordChange}
-                            renderItem={renderItem}
-                            filters={renderFilters()}
-                        />
-                    )}
-                </>
-            );
-        }
-    }
-
     return (
         <>
-            {FilteredItemsWithLoadingIndicator()}
+            <h1>Translations</h1>
         </>
     );
 }
