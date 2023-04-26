4169f4f51aa78409c1940cc204fbb7f188b86151
lsprr <16653744+lsprr@users.noreply.github.com>
2023-04-25 20:55:36 -0400

    Update Villagers page

--- frontend/src/pages/villagers.tsx
+++ frontend/src/pages/villagers.tsx
@@ -1,19 +1,66 @@
+import { fetchData } from '@/utils/api';
+import { mergeAllArrays } from '@utils/arrayUtils';
 import { useEffect, useState } from 'react';
+import { ItemGrids } from '@/components/ItemGrids';
+import { Card } from '@/components/Card';
+import { ErrorAPI } from '@/components/Errors/API';
+import { Loading } from '@/components/Loading';
 
-export default function Villagers() {
-    const [category, setCategory] = useState({});
+type StaticImageData = {
+    src: string;
+    height: number;
+    width: number;
+    blurDataURL?: string;
+}
+
+type Village = {
+    name: string;
+    iconImage: StaticImageData;
+};
+
+type DataFetchProps = {
+    category: string;
+};
+
+const DataFetch = ({ category }: DataFetchProps) => {
+    const [displayedItems, setDisplayedItems] = useState<Array<Village> | null>(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [errorMessage, setErrorMessage] = useState<Error | null>(null);
 
     useEffect(() => {
-        fetch('http://localhost:3001/api/villagers')
-            .then((response) => response.json())
+        fetchData(category)
             .then((data) => {
-                setCategory({ data })
+                setDisplayedItems(mergeAllArrays(data));
+                setIsLoading(false);
+            })
+            .catch((error) => {
+                setErrorMessage(new Error(`Error fetching data. Please try again.`));
+                console.error(`Error fetching ${category.toUpperCase}:`, error);
             });
-    }, []);
+    }, [category]);
+
+    const renderItem = (item: Village) => {
+        return (
+            <Card
+                name={item.name}
+                image={item.iconImage}
+            />
+        );
+    };
 
     return (
         <>
-            <h1>Villagers</h1>
+            {errorMessage ? (
+                <ErrorAPI />
+            ) : isLoading ? (
+                <Loading />
+            ) : (
+                <ItemGrids data={displayedItems} renderItem={renderItem} />
+            )}
         </>
     );
+}
+
+export default function Villagers() {
+    return <DataFetch category="villagers" />;
 }
