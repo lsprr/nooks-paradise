5120f2340cadbfe63f4c970b901760da9693c393
lsprr <16653744+lsprr@users.noreply.github.com>
2023-05-02 17:23:42 -0400

    Add responsive design for achievement table on mobile devices

--- src/pages/achievements.tsx
+++ src/pages/achievements.tsx
@@ -20,8 +20,8 @@ const fetchFunction = () => {
 
 const renderHeader = () => (
     <tr>
-        <th colSpan={1} className="w-1/3 p-2 rounded-tl-2xl">Name &amp; Description</th>
-        <th colSpan={3} className="p-2 rounded-tr-2xl">Details &amp; Rewards</th>
+        <th colSpan={1} className="w-full md:w-1/3 p-2 rounded-tl-2xl text-xs md:text-base">Name &amp; Description</th>
+        <th colSpan={3} className="p-2 rounded-tr-2xl text-xs md:text-base">Details &amp; Rewards</th>
     </tr>
 );
 
@@ -30,16 +30,16 @@ const renderBody = (item: Achievement, index: number) => {
     return (
         <>
             <tr key={`${index}-title`} className="border-t border-b border-dashed border-darkGray dark:border-white">
-                <td rowSpan={tiersLength + 1} className="p-2 rounded-bl-2xl border-r border-dashed border-darkGray dark:border-white">
+                <td rowSpan={tiersLength + 1} className="p-1 md:p-2 rounded-bl-2xl border-r border-dashed border-darkGray dark:border-white text-xs md:text-base">
                     <b>{item.name}</b> <br /> {item.achievementDescription}
                 </td>
-                <td colSpan={3} className="p-2 border-t border-dashed border-darkGray dark:border-white">
+                <td colSpan={3} className="p-1 md:p-2 border-t border-dashed border-darkGray dark:border-white text-xs md:text-base">
                     <b>Criteria</b><br /> {item.achievementCriteria}
                 </td>
             </tr>
             {Object.entries(item.tiers).map(([key, tier], tierIndex) => (
                 <tr key={`${index}-${key}`}>
-                    <td className="p-2">
+                    <td className="p-1 md:p-2 text-xs md:text-base">
                         {tierIndex === 0 && (
                             <div className="font-bold">Milestones</div>
                         )}
@@ -49,20 +49,20 @@ const renderBody = (item: Achievement, index: number) => {
                             </li>
                         </ol>
                     </td>
-                    <td className="p-2">
+                    <td className="p-1 md:p-2 text-xs md:text-base">
                         {tierIndex === 0 && (
                             <div className="font-bold">Passport Titles</div>
                         )}
                         <ol>
                             {tier.nouns.map((noun, i) => (
                                 <li key={`${noun}-${i}`}>
-                                    <span className="bg-green-100 text-green-700 border border-green-300 border-r-0 rounded-l px-1 py-0.5 mr-1 inline-block">{tier.modifier}</span>
-                                    <span className="bg-green-100 text-green-700 border border-green-300 border-l-0 rounded-r px-1 py-0.5 inline-block">{noun}</span>
+                                    <span className="bg-green-100 text-green-700 border border-green-300 border-r-0 rounded-l px-1 py-0.5 mr-1 inline-block text-xs md:text-sm">{tier.modifier}</span>
+                                    <span className="bg-green-100 text-green-700 border border-green-300 border-l-0 rounded-r px-1 py-0.5 inline-block text-xs md:text-sm">{noun}</span>
                                 </li>
                             ))}
                         </ol>
                     </td>
-                    <td className="p-2">
+                    <td className="p-1 md:p-2 text-xs md:text-base">
                         {tierIndex === 0 && (
                             <div className="font-bold">Nook Miles</div>
                         )}
