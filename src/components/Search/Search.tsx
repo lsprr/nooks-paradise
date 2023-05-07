2a9dadbdb846580d6ebb3c932c2e0ca9eb7b232b
lsprr <16653744+lsprr@users.noreply.github.com>
2023-05-07 15:03:01 -0400

    Minor correction

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -7,7 +7,7 @@ export const Search = ({ onSearchItem }: SearchProps) => {
         <div className="flex items-end justify-between bg-white p-6 mt-8 rounded-2xl md:w-1/3">
             <label
                 htmlFor="search"
-                className="w-full overflow-hidden border-gray-200 shadow-sm border border-1-darkGray focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600 flex flex-col items-center h-full rounded-2xl"
+                className="p-4 md:p-0 w-full overflow-hidden border-gray-200 shadow-sm border border-1-darkGray focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600 flex flex-col items-center h-full rounded-2xl"
             >
                 <span className="text-base text-darkGray capitalize">Search</span>
 
