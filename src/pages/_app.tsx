a81746b657f8537afa6f1cff77520147c4a95df5
lsprr <16653744+lsprr@users.noreply.github.com>
2023-05-11 21:16:45 -0400

    Update Open Graph meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -27,9 +27,21 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Wilbur</title>
+        <meta name="description" content="This is your one-stop co-pilot for all things Animal Crossing: New Horizons! We got you covered on neighbors, island life, fashion, DIY, critters, K.K. Slider tunes, and special events. So buckle up, and let's fly through the fantastic world of ACNH together!" />
+
+        <meta property="og:url" content="https://wilbur.vercel.app" />
+        <meta property="og:type" content="website" />
         <meta property="og:title" content="Wilbur" />
-        <meta name="description" content="This is your one-stop co-pilot for all things Animal Crossing: New Horizons! We got you covered on neighbors, island life, fashion, DIY, critters, K.K. Slider tunes, and special events. So buckle up, and let's fly through the fantastic world of ACNH together!" key="desc" />
         <meta property="og:description" content="This is your one-stop co-pilot for all things Animal Crossing: New Horizons! We got you covered on neighbors, island life, fashion, DIY, critters, K.K. Slider tunes, and special events. So buckle up, and let's fly through the fantastic world of ACNH together!" />
+        <meta property="og:image" content="" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="wilbur.vercel.app" />
+        <meta property="twitter:url" content="https://wilbur.vercel.app" />
+        <meta name="twitter:title" content="Wilbur" />
+        <meta name="twitter:description" content="This is your one-stop co-pilot for all things Animal Crossing: New Horizons! We got you covered on neighbors, island life, fashion, DIY, critters, K.K. Slider tunes, and special events. So buckle up, and let's fly through the fantastic world of ACNH together!" />
+        <meta name="twitter:image" content="" />
+
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
