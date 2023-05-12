7df110f38345c05cb20ea4b6e47159ae25591e59
lsprr <16653744+lsprr@users.noreply.github.com>
2023-05-12 01:57:03 -0400

    Fix flickering issue

--- src/components/ItemGrid/ItemGrid.tsx
+++ src/components/ItemGrid/ItemGrid.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import { NoItemsFound } from "../Error/NoItemsFound";
 import { Container } from "../Layout/Container";
+import { Loading } from "@components/Loading/Loading";
 
 type ItemGridProps<T> = {
     data: Array<T> | null,
@@ -12,18 +13,28 @@ export const ItemGrid = <T extends {}>({ data, renderItem }: ItemGridProps<T>): 
         <Container>
             <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
                 <div className="lg:col-span-4">
-                    {data && data.length >= 0 ? (
-                        <ul className="grid gap-4 mt-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-                            {data.map((item, index) => {
-                                const renderedItem = renderItem && renderItem(item);
-                                return renderedItem ? (
-                                    <li key={index}>{renderedItem}</li>
-                                ) : [];
-                            })}
-                        </ul>
-                    ) : (
-                        <NoItemsFound />
-                    )}
+                    {(() => {
+                        if (data && data.length > 0) {
+                            return (
+                                <ul className="grid gap-4 mt-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                                    {data.map((item, index) => {
+                                        const renderedItem = renderItem && renderItem(item);
+                                        return renderedItem &&
+                                            <li key={index}>{renderedItem}</li>
+
+                                    })}
+                                </ul>
+                            )
+                        } else if (data && data.length === 0) {
+                            return (
+                                <Loading />
+                            )
+                        } else {
+                            return (
+                                <NoItemsFound />
+                            )
+                        }
+                    })()}
                 </div>
             </div>
         </Container>
