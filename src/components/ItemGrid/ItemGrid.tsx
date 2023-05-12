89d625a7c79dfeac2cfe987dca2dffd5f5899700
lsprr <16653744+lsprr@users.noreply.github.com>
2023-05-12 01:52:57 -0400

    Minor correction

--- src/components/ItemGrid/ItemGrid.tsx
+++ src/components/ItemGrid/ItemGrid.tsx
@@ -12,7 +12,7 @@ export const ItemGrid = <T extends {}>({ data, renderItem }: ItemGridProps<T>): 
         <Container>
             <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
                 <div className="lg:col-span-4">
-                    {data && data.length > 0 ? (
+                    {data && data.length >= 0 ? (
                         <ul className="grid gap-4 mt-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
                             {data.map((item, index) => {
                                 const renderedItem = renderItem && renderItem(item);
