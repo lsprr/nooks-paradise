d5c02e0f5fef97d2f2aa0d18f6dcbeb19b2a94fb
lsprr <16653744+lsprr@users.noreply.github.com>
2023-05-14 00:34:25 -0400

    Update Loading component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,7 +3,7 @@ import DAL from '@assets/gifs/DAL.gif';
 
 export const Loading = () => {
     return (
-        <div className="fixed inset-0 flex items-center justify-center">
+        <div className="fixed inset-0 flex items-center justify-center bg-brightBlue">
             <Image src={DAL} alt='Loading' loading='eager' className='object-scale-down' />
         </div>
     )
